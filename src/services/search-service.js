60dbe8b64b4e91b989a24dccb7c08f8410d998d0
K Vivekananda Reddy <vivekananda-reddy@users.noreply.github.com>
2023-04-22 13:33:09 -0400

    minor changes

--- src/services/search-service.js
+++ src/services/search-service.js
@@ -1,27 +1,27 @@
 import axios from "axios";
-
+const api = axios.create({ withCredentials: true });
 const API_BASE =  process.env.REACT_APP_API_BASE;
 export const searchByMeal = async (searchString) => {
-    const response = await axios.get(`${API_BASE}/search/${searchString}`)
+    const response = await api.get(`${API_BASE}/search/${searchString}`)
     return response.data
 }
 
 export const getMealDetails = async (mealId) => {
-    const response = await axios.get(`${API_BASE}/details/${mealId}`)
+    const response = await api.get(`${API_BASE}/details/${mealId}`)
     return response.data
 }
 
 export const getCategories = async () => {
-    const response = await axios.get(`${API_BASE}/categories`)
+    const response = await api.get(`${API_BASE}/categories`)
     return response.data
 }
 
 export const getMealsbyCategory = async (categoryName) => {
-    const response = await axios.get(`${API_BASE}/categories/${categoryName}`)
+    const response = await api.get(`${API_BASE}/categories/${categoryName}`)
     return response.data
 }
 
 export const getRandomMeal = async () => {
-    const response = await axios.get(`${API_BASE}/random`)
+    const response = await api.get(`${API_BASE}/random`)
     return response.data
 }
